e99ae1ff29ec33f7a671c8171a91cb641d434714
Jack Gindi <gindije@gmail.com>
2025-04-25 08:00:09 -0400

    reward func

--- rl_utils.py
+++ rl_utils.py
@@ -8,12 +8,13 @@ def compute_reward(
 ) -> float:
     if prev_info is None:
         return base_reward
+    if base_reward == 0:
+        base_reward = -2
     lives_reward = -100 if info["life"] < prev_info["life"] else 0
     score_reward = (info["score"] - prev_info["score"]) / 100
     if info["status"] != prev_info["status"]:
         status_reward = 100 if info["status"] != "small" else 0
     else:
         status_reward = 5 if info["status"] != "small" else 0
-    # Print reward components for debugging
     total_reward = base_reward + lives_reward + score_reward + status_reward
     return total_reward
